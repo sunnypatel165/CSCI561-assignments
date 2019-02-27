abd3c293e2a64dd1c0fa643607b44b1f48d92fb3
sunnypatel165 <sunnydep@usc.edu>
2019-02-27 10:32:48 -0800

    input

--- Assignment_2/attempt1.py
+++ Assignment_2/attempt1.py
@@ -14,9 +14,29 @@ class Flight:
           str(self.max_service_time) + " ")
 
 
+def print_flights(flights):
+    for i in flights:
+        i.print_flight()
+
+
+def read_file():
+    f1 = open("input0.txt", "r")
+    line = f1.readline().strip().split()
+    landing, gates, takingoff = int(line[0]), int(line[1]), int(line[2])
+    n = int(f1.readline())
+    flights = []
+    for i in xrange(n):
+        line = f1.readline().strip().split()
+        flight = Flight(int(line[0]), int(line[1]), int(line[2]), int(line[3]), int(line[4]))
+        flights.append(flight)
+    print_flights(flights)
+
+
 def main():
     flight = Flight(0, 10, 50, 20, 70)
     flight.print_flight()
+
+    read_file()
 
 
 if __name__ == '__main__':
